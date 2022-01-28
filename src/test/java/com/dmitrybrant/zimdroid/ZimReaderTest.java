5153f1c8547af0c0032449880f82fb3a3e081e0d
Dmitry Brant <me@dmitrybrant.com>
2022-01-28 15:24:42 -0500

    Fix additional lint warnings.

--- src/test/java/com/dmitrybrant/zimdroid/ZimReaderTest.java
+++ src/test/java/com/dmitrybrant/zimdroid/ZimReaderTest.java
@@ -21,14 +21,13 @@ public class ZimReaderTest {
     private static final String ZIM_MALFORMED_HEADER = "malformed_header.zim";
     private static final String ZIM_NO_CONTENT_AFTER_HEADER = "cut_off_after_header.zim";
 
+    @SuppressWarnings("unchecked")
     @Mock LruCache<Integer, DirectoryEntry> mockCache = mock(LruCache.class);
 
     @Test
     public void testZimReaderMetadata() throws Exception {
         when(mockCache.get(any(Integer.TYPE))).thenReturn(null);
-        ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + TEST_ZIM_FILE), mockCache, mockCache);
-        try {
-
+        try (ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + TEST_ZIM_FILE), mockCache, mockCache)) {
             assertEquals(reader.getZimTitle(), "Wikipedia");
             assertEquals(reader.getZimDescription(), "From Wikipedia, the free encyclopedia");
             assertEquals(reader.getZimDate(), new SimpleDateFormat("yyyy-MM-dd", Locale.ROOT).parse("2015-06-02"));
@@ -37,33 +36,23 @@ public class ZimReaderTest {
 
             String mainTitle = reader.getMainPageTitle();
             assertEquals(mainTitle, "Summary");
-
-        } finally {
-            reader.close();
         }
     }
 
     @Test
     public void testZimReaderPrefixSearch() throws Exception {
         when(mockCache.get(any(Integer.TYPE))).thenReturn(null);
-        ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + TEST_ZIM_FILE), mockCache, mockCache);
-        try {
-
+        try (ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + TEST_ZIM_FILE), mockCache, mockCache)) {
             List<String> results = reader.searchByPrefix("R", 2);
             assertEquals(results.size(), 2);
             assertEquals(results.get(0), "Raelette");
-
-        } finally {
-            reader.close();
         }
     }
 
     @Test
     public void testZimReaderGetArticleContent() throws Exception {
         when(mockCache.get(any(Integer.TYPE))).thenReturn(null);
-        ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + TEST_ZIM_FILE), mockCache, mockCache);
-        try {
-
+        try (ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + TEST_ZIM_FILE), mockCache, mockCache)) {
             String normalizedTitle = reader.getNormalizedTitle("A Fool for You");
             assertEquals(normalizedTitle, "A Fool for You");
 
@@ -79,67 +68,59 @@ public class ZimReaderTest {
             html = reader.getDataForTitle(normalizedTitle).toString("utf-8");
             assertTrue(html.startsWith("<html>"));
             assertTrue(html.endsWith("</html>"));
-
-        } finally {
-            reader.close();
         }
     }
 
     @Test
     public void testZimReaderGetMediaContent() throws Exception {
         when(mockCache.get(any(Integer.TYPE))).thenReturn(null);
-        ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + TEST_ZIM_FILE), mockCache, mockCache);
-        try {
-
+        try (ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + TEST_ZIM_FILE), mockCache, mockCache)) {
             reader.setLzmaDictSize(2 * 1024 * 1024);
 
             byte[] bytes = reader.getDataForUrl("I/m/Ray_C._Geor.jpg").toByteArray();
             assertEquals(bytes[0], (byte) 0xFF);
             assertEquals(bytes[bytes.length - 1], (byte) 0xD9);
-
-        } finally {
-            reader.close();
         }
     }
 
     @Test
-    public void testZimReaderZeroLength() throws Exception {
+    public void testZimReaderZeroLength() {
         try {
             new ZimReader(new ZimFile(RAW_DIR + ZIM_ZERO_BYTES), mockCache, mockCache);
-            assertTrue("Should not reach this point.", false);
+            fail("Should not reach this point.");
         } catch (IOException e){
             //
         }
     }
 
     @Test
-    public void testZimReaderMalformedHeader() throws Exception {
+    public void testZimReaderMalformedHeader() {
         try {
             new ZimReader(new ZimFile(RAW_DIR + ZIM_MALFORMED_HEADER), mockCache, mockCache);
-            assertTrue("Should not reach this point.", false);
+            fail("Should not reach this point.");
         } catch (IOException e){
             //
         }
     }
 
     @Test
-    public void testZimReaderCorruptHeader() throws Exception {
+    public void testZimReaderCorruptHeader() {
         try {
             new ZimReader(new ZimFile(RAW_DIR + ZIM_CORRUPT_HEADER), mockCache, mockCache);
-            assertTrue("Should not reach this point.", false);
+            fail("Should not reach this point.");
         } catch (IOException e){
             //
         }
     }
 
     @Test
-    public void testZimReaderNoContentAfterHeader() throws Exception {
+    public void testZimReaderNoContentAfterHeader() {
         try {
             when(mockCache.get(any(Integer.TYPE))).thenReturn(null);
             ZimReader reader = new ZimReader(new ZimFile(RAW_DIR + ZIM_NO_CONTENT_AFTER_HEADER), mockCache, mockCache);
 
             reader.getZimTitle();
-            assertTrue("Should not reach this point.", false);
+            fail("Should not reach this point.");
         } catch (IOException e){
             //
         }
